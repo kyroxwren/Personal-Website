719f45a45088293f0d0bca3dacc1f5d4d4cb4d99
kyroxwren <lebogang15@icloud.com>
2024-12-08 18:42:41 +0200

    1st commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    distDir: 'dist',
+    images: {
+        unoptimized: true
+    },
+   
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,  
+  };
+  
+module.exports = nextConfig
